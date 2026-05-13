2538edde00033819a3bad9a0bd250cebd75bb451
merveyasar <merve.yasar.ce@gmail.com>
2026-05-13 21:37:46 +0300

    feat: esbuild yerine importmap'e geçildi ve flash bildirimleri kuruldu

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,10 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+
+// Importmap sayesinde doğrudan isimle çağırabiliyoruz
+import Notification from "@stimulus-components/notification"
+
+// Manuel kayıt yapalım
+application.register("notification", Notification)
